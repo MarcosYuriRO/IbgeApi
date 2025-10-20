d9b0b35a05d352eb283f480b12ca4a03c7a88e5f
Yuri <marcos.yuriro@gmail.com>
2025-10-20 19:54:07 -0300

    First Commit

---
+++ src/test/java/com/marcos/ProjetoIbgeApi/ProjetoIbgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.ProjetoIbgeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIbgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
